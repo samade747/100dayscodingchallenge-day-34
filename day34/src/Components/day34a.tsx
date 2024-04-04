5b609f73a411d5c5c5aaae7cfc2fea6a9aae841f
samade747 <samad.e747@gmail.com>
2024-04-04 20:17:56 +0500

    817pm 4 4 2024

---
+++ day34/src/Components/day34a.tsx
@@ -0,0 +1,15 @@
+const squarroot: number = Math.sqrt(25);
+console.log(squarroot)
+
+
+function getRandomInt(): number {
+    return Math.floor(Math.random() * 10)
+}
+
+
+console.log(getRandomInt())
+
+
+const difference: number = Math.abs(-5 -5)
+
+console.log(difference)
